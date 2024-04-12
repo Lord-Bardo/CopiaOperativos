3b06eafd26b9c16d9b2b2ab20d70c34418021c0a
vibello <vibello@frba.utn.edu.ar>
2024-04-12 16:35:03 +0000

    :)

--- entradasalida/src/entradasalida.c
+++ entradasalida/src/entradasalida.c
@@ -2,5 +2,43 @@
 
 int main(int argc, char* argv[]) {
     decir_hola("una Interfaz de Entrada/Salida");
+
+    char *tipo_interfaz;
+	char *tiempo_unidad_trabajo;
+	char *ip_kernel;
+    char *puerto_kernel;
+    char *ip_memoria;
+    char *puerto_memoria;
+    char *path_base_dialfs;
+    char *block_size;
+    char *block_count;
+
+    t_config *config;
+    tipo_interfaz = config_get_string_value(config, "TIPO_INTERFAZ");
+	tiempo_unidad_trabajo = config_get_string_value(config, "TIEMPO_UNIDAD_TRABAJO");
+	ip_kernel = config_get_string_value(config, "IP_KERNEL");
+    puerto_kernel = config_get_string_value(config, "PUERTO_KERNEL");
+    ip_memoria = config_get_string_value(config, "IP_MEMORIA");
+    puerto_memoria = config_get_string_value (config, "PUERTO_MEMORIA")
+    path_base_dialfs = config_get_string_value (config, "PATH_BASE_DIALFS")
+    block_size = config_get_string_value (config, "BLOCK_SIZE")
+    block_count = config_get_string_value (config, "BLOCK_COUNT")
+
+   t_config* iniciar_config(void) {
+	t_config *nuevo_config;
+	nuevo_config = config_create("memoria.config");
+	if (nuevo_config == NULL){
+		printf("No se pudo crear el config.");
+		exit(2);
+	}
+
+	return nuevo_config;
+}
+
+void terminar_programa(t_config* config){
+    if (config != NULL){
+		config_destroy(config);
+	}
+}
     return 0;
 }
--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -2,5 +2,69 @@
 
 int main(int argc, char* argv[]) {
     decir_hola("Kernel");
+
+    char *puerto_escucha;
+    char *ip_memoria;
+    char *puerto_memoria;
+    char *ip_cpu;
+    char *puerto_cpu_dispatch;
+    char *puerto_cpu_interrupt;
+    char *algoritmo_planificacion;
+    char *quantum;
+    char *recursos;
+    char *instancias_recursos;
+    char *grado_multiprogramacion;
+
+    t_config *config;
+
+    //  ARCHIVOS DE CONFIGURACION
+
+    puerto_escucha = config_get_string_value(config, "PUERTO_ESCUCHA");
+    ip_memoria = config_get_string_value(config, "IP_MEMORIA");
+    puerto_memoria = config_get_string_value(config, "PUERTO_MEMORIA");
+    ip_cpu = config_get_string_value(config, "IP_CPU");
+    puerto_cpu_dispatch = config_get_string_value(config, "PUERTO_CPU_DISPATCH");
+    puerto_cpu_interrupt = config_get_string_value(config, "PUERTO_CPU_INTERRUPT");
+    algoritmo_planificacion = config_get_string_value(config, "ALGORITMO_PLANIFICACION");
+    quantum = config_get_string_value(config, "QUANTUM");
+    recursos = config_get_string_value(config, "RECURSOS");
+    instancias_recursos = config_get_string_value(config, "INSTANCIAS_RECURSOS");
+    grado_multiprogramacion = config_get_string_value(config, "GRADO_MULTIPROGRAMACION");
+
+        #include "cpu.h"
+
+    // Muestro en pantalla el valor de QUANTUM
+    printf("%s", quantum);
+
+    terminar_programa(config);
+
+    return 0;
+}
+
+t_config *iniciar_config(void)
+{
+	t_config *nuevo_config;
+	nuevo_config = config_create("kernel.config"); 
+	if (nuevo_config == NULL){
+		printf("No se pudo crear el config.");
+		exit(2);
+	}
+
+	return nuevo_config;
+}
+
+void terminar_programa(t_config *config)
+{
+	if (config != NULL){
+		config_destroy(config);
+	}
+}
+
+
+
+
+
+
+
     return 0;
 }
