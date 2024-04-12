10c110c853f87eed910ca15e52086033723150e0
vibello <vibello@frba.utn.edu.ar>
2024-04-12 20:34:01 +0000

    :

--- entradasalida/src/entradasalida.c
+++ entradasalida/src/entradasalida.c
@@ -14,24 +14,26 @@ int main(int argc, char* argv[]) {
     char *block_count;
 
     t_config *config;
+
+    config = iniciar_config();
+
     tipo_interfaz = config_get_string_value(config, "TIPO_INTERFAZ");
 	tiempo_unidad_trabajo = config_get_string_value(config, "TIEMPO_UNIDAD_TRABAJO");
 	ip_kernel = config_get_string_value(config, "IP_KERNEL");
     puerto_kernel = config_get_string_value(config, "PUERTO_KERNEL");
     ip_memoria = config_get_string_value(config, "IP_MEMORIA");
     puerto_memoria = config_get_string_value (config, "PUERTO_MEMORIA");
-    path_base_dialfs = config_get_string_value (config, "PATH_BASE_DIALFS")
-    block_size = config_get_string_value (config, "BLOCK_SIZE")
-    block_count = config_get_string_value (config, "BLOCK_COUNT")
+    path_base_dialfs = config_get_string_value (config, "PATH_BASE_DIALFS");
+    block_size = config_get_string_value (config, "BLOCK_SIZE");
+    block_count = config_get_string_value (config, "BLOCK_COUNT");
 
-   t_config* iniciar_config(void) {
+    t_config* iniciar_config(void);
 	t_config *nuevo_config;
 	nuevo_config = config_create("memoria.config");
 	if (nuevo_config == NULL){
 		printf("No se pudo crear el config.");
 		exit(2);
 	}
-
 	return nuevo_config;
 }
 
@@ -39,6 +41,4 @@ void terminar_programa(t_config* config){
     if (config != NULL){
 		config_destroy(config);
 	}
-}
-    return 0;
 }
--- entradasalida/src/entradasalida.h
+++ entradasalida/src/entradasalida.h
@@ -1,11 +1,19 @@
 #ifndef ENTRADASALIDA_H_
 #define ENTRADASALIDA_H_
-
 #include <stdlib.h>
 #include <stdio.h>
-#include <utils/hello.h>
+#include <commons/log.h>
+#include <commons/string.h>
+#include <commons/config.h>
+#include <readline/readline.h>
 
+#include <utils/conexion.h>
+
+t_log* iniciar_logger(void);
 t_config* iniciar_config(void);
-void terminar_programa(t_config* config);
+void leer_consola(t_log*);
+void paquete(int);
+void terminar_programa(int, t_log*, t_config*);
 
 #endif
+
