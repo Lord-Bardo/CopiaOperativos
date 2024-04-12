43c2cd24a5a448eff82f735b5a03804825a566ec
¨MatyTort01¨ <mtort@frba.utn.edu.ar>
2024-04-12 14:58:22 +0000

    cpu_config

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -1,6 +1,36 @@
 #include "cpu.h"
 
 int main(int argc, char* argv[]) {
-    decir_hola("CPU");
+    t_config* config;
+    char* ip;
+    
+    // Inicio el config
+    config = iniciar_config();
+
+    // Obtengo el valor del campo IP del archivo cpu.config
+    ip = config_get_string_value(config, "IP");
+
+    // Muestro en pantalla el valor de IP
+    printf("%s", ip);
+
+    terminar_programa(config);
+
     return 0;
+}
+
+t_config* iniciar_config(void) {
+	t_config *nuevo_config;
+	nuevo_config = config_create("cpu.config");
+	if (nuevo_config == NULL){
+		printf("No se pudo crear el config.");
+		exit(2);
+	}
+
+	return nuevo_config;
+}
+
+void terminar_programa(t_config* config){
+    if (config != NULL){
+		config_destroy(config);
+	}
 }
