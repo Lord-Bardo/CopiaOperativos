53877f57d4ce18384274697ff8fc5ac26864a246
¨MatyTort01¨ <mtort@frba.utn.edu.ar>
2024-04-26 23:03:25 +0000

    .

--- utils/src/utils/planificadores.c
+++ utils/src/utils/planificadores.c
@@ -1,16 +1,7 @@
 #include <utils/planificadores.h>
 
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-struct{
-   
-=======
-=======
->>>>>>> 7653b76 (Actualizo denuevo)
 typedef struct{
-    __uint32_t pc;
->>>>>>> 9519930d52bcdfc5bfcf5c20b69baaee0325214a
+    __uint32_t PC;
     __uint8_t AX;
     __uint8_t BX;
     __uint8_t CX;
@@ -23,9 +14,8 @@ typedef struct{
     __uint32_t DI;  //contiene la direccion logica de memoria de destino desde donde se va a copir un string
 }t_registros;
 
-
 typedef struct{
-    int pid;
+    int PID;
     int quantum;
     t_registros registros;
 }t_pcb;
