6b95863ff43b1fe6d7df35740ffbe9ad73b458f2
Lucho <lucianotomasriente@gmail.com>
2024-06-17 23:20:34 +0000

    ENTRADASALIDA: Interfaz genérica funcionando

--- entradasalida/src/entradasalida_interfaces.c
+++ entradasalida/src/entradasalida_interfaces.c
@@ -3,10 +3,12 @@
 // El valor de la cant_unidades_trabajo viene de kernel y se multiplica por el valor que tiene el config de tiempo_unidad_trabajo
 void interfaz_generica(int cant_unidades_trabajo) {
     int tiempo_unidad_trabajo = TIEMPO_UNIDAD_TRABAJO; // Tomar el valor del config
-    int tiempo_total = cant_unidades_trabajo * tiempo_unidad_trabajo;
+    int tiempo_total = (cant_unidades_trabajo * tiempo_unidad_trabajo) / 1000;
 
     // Simular el tiempo de espera
-    sleep(tiempo_total * 1000); // El *1000 es para pasar la unidad de trabajo a microsegundos
+    log_info(entradasalida_logger, "E/S: haciendo un sleep");
+    sleep(tiempo_total);
+    log_info(entradasalida_logger, "E/S: deje de hacer un sleep");
 }
 
 // void interfaz_stdin(/* Registro Dirección, Registro Tamaño */){
