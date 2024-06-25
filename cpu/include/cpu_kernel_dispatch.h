4f087c06997964345f0fe474fd3d6f63cfde2fc5
Lord-Bardo <mlombardo@frba.utn.edu.ar>
2024-06-25 03:46:41 +0000

    Extrañamente compila lo que hice a la primera, tengo que probar que haga lo que quiero todavia

--- cpu/include/cpu_kernel_dispatch.h
+++ cpu/include/cpu_kernel_dispatch.h
@@ -1,23 +1,11 @@
 #ifndef CPU_KERNEL_DISPATCH_H_
 #define CPU_KERNEL_DISPATCH_H_
 
-#include "cpu_gestor.h"
-#include "instrucciones.h"
+
+#include "cpu_utils.h"
 
 
-void atender_cpu_kernel_dispatch(void);
 void iniciar_ciclo_instruccion(t_pcb);
-void buffer_desempaquetar_registros(t_buffer *buffer, t_registros *registros);
-int pcb_get_pid(t_pcb *pcb);
-void buffer_desempaquetar_pcb(t_buffer *buffer, t_pcb* pcb);
-void agregar_contexto_ejecucion_a_paquete(t_paquete *paquete, t_pcb* pcb);
-void agregar_registros_a_paquete(t_paquete *paquete, t_registros registros);
-
-void agregar_uint32_a_paquete(t_paquete *paquete, uint32_t n);
-void agregar_uint8_a_paquete(t_paquete *paquete, uint8_t n);
-void agregar_pid_a_paquete(t_paquete *paquete, int pid);
-int pcb_get_pid(t_pcb *pcb);
-t_registros pcb_get_registros(t_pcb *pcb);
 
 
 
