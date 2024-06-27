cd7ad641b5e954eb5d147010794aabcccc29f792
vibello <vibello@frba.utn.edu.ar>
2024-06-27 21:26:00 +0000

    no aguanto mas memoria

--- memoria/include/memoria_entradasalida.h
+++ memoria/include/memoria_entradasalida.h
@@ -4,21 +4,29 @@
 #include "memoria_utils.h"
 
 void atender_memoria_entradasalida(void);
-/* 
+
 // Variables globales
 extern void* espacio_usuario; // Memoria contigua simulada
 extern int fd_entradasalida; // File descriptor de entrada/salida
 extern t_log* memoria_logger; // Logger para el módulo de memoria
 
 // Funciones
-t_log* memoria_logger = log_create("memoria.log", "MEMORIA", 1, LOG_LEVEL_DEBUG);
+//t_log* memoria_logger = log_create("memoria.log", "MEMORIA", 1, LOG_LEVEL_DEBUG);
 
 t_pcb_memoria* obtener_proceso(int pid);
 
 int escribir_memoria(t_pcb_memoria* proceso, int num_pagina, int offset, void* buffer, int tamanio);
 int leer_memoria(t_pcb_memoria* proceso, int num_pagina, int offset, void* buffer, int tamanio);
 
+// Declaración de las variables globales
+extern int cantidad_procesos;
+
+// Declaración de la función obtener_proceso
 t_pcb_memoria* obtener_proceso(int pid);
- */
+
+extern t_pcb_memoria procesos[MAX_PROCESOS]; // Declaración externa
+extern int cantidad_procesos; // Declaración externa
+
+void enviar_datos(int socket, void* buffer, int size);
 
 #endif
--- memoria/src/memoria_entradasalida.c
+++ memoria/src/memoria_entradasalida.c
@@ -1,30 +1,37 @@
 #include "../include/memoria_entradasalida.h"
-/* 
-int escribir_memoria (t_pcb_memoria proceso, int num_pagina, int offset, void* buffer, int tamanio)
+#define MAX_PROCESOS 100 // Define el número máximo de procesos CHEQUEAR
+// Definición de las variables globales
+t_pcb_memoria procesos[MAX_PROCESOS]; // Define el tamaño máximo adecuado para tus necesidades
+int cantidad_procesos = 0;
+
+// Declaración de la función obtener_proceso
+t_pcb_memoria* obtener_proceso(int pid);
+
+int escribir_memoria (t_pcb_memoria* proceso, int num_pagina, int offset, void* buffer, int tamanio)
 {
-	if (num_pagina >= proceso.cant_paginas) { //primero verificamos que no hayan errores
+	if (num_pagina >= proceso->cant_paginas) { //primero verificamos que no hayan errores
         fprintf(stderr, "Número de página fuera de rango\n");
         return -1;
     }
-	t_pagina* pagina = &proceso.tabla_paginas[num_pagina];
+	t_pagina* pagina = &proceso->tabla_paginas[num_pagina];
     if (!pagina->bit_presencia) {
         fprintf(stderr, "Página no presente en memoria\n");
         return -1;
     }
 	// Calcular la dirección en la memoria contigua
     int direccion_memoria = pagina ->num_frame * TAM_PAGINA + offset;
-    memcpy(espacio_usuario + direccion_memoria, buffer, tamanio);
+    memcpy(espacio_usuario + direccion_memoria, buffer, tamanio); //copia datos hacia la memoria del proceso desde el buffer.(lo toma como fuente)
     return 0;
 
 }
 
-int leer_memoria (t_pcb_memoria proceso, int num_pagina, int offset, void* buffer, int tamanio) 
+int leer_memoria (t_pcb_memoria* proceso, int num_pagina, int offset, void* buffer, int tamanio) 
 {
-    if (num_pagina >= proceso.cant_paginas) { //primero verificar posibles errores
+    if (num_pagina >= proceso->cant_paginas) { //primero verificar posibles errores
         fprintf(stderr, "Número de página fuera de rango\n");
         return -1;
 
-    t_pagina* pagina = proceso.tabla_paginas[num_pagina];
+    t_pagina* pagina = &proceso->tabla_paginas[num_pagina];
     if (!pagina->bit_presencia) {
         fprintf(stderr, "Página no presente en memoria\n");
         return -1;
@@ -32,10 +39,11 @@ int leer_memoria (t_pcb_memoria proceso, int num_pagina, int offset, void* buffe
 
     // Calcular la dirección en la memoria contigua
     int direccion_memoria = pagina->num_frame * TAM_PAGINA + offset;
-    memcpy(buffer, espacio_usuario + direccion_memoria, tamanio);
+    memcpy(buffer, espacio_usuario + direccion_memoria, tamanio); //copia datos desde la memoria del proceso hacia el buffer.(lo toma como destino)
+
     return 0;
 }
-*/
+
 void atender_memoria_entradasalida(){
     int continuar = 1;
 	while( continuar ){
@@ -43,18 +51,24 @@ void atender_memoria_entradasalida(){
 		t_buffer *buffer = crear_buffer();
 		recibir_paquete(fd_entradasalida, &cod_op, buffer);
 		switch(cod_op){
-            /*
 			//case SOLICITUD_ACCESO_TABLAS_PAGINAS: //POSIBLE NOMBRE DEL MENSAJE 
 			case SOLICITUD_ESCRITURA: //desp confirmar el nombre con lucho (YA LO PUSE EN EL CONEXION.H){
-                t_pcb_memoria* solicitud;
-				buffer_desempaquetar_proceso(buffer, &solicitud);
-				t_pcb_memoria* proceso_recibido = obtener_proceso(solicitud->pid);
+                t_pcb_memoria solicitud;
+                int tamanio;
+                void* data;
+
+                buffer_desempaquetar_proceso(buffer, &solicitud);
+                buffer_desempaquetar(buffer, &tamanio);
+                data = malloc(tamanio);
+                buffer_desempaquetar(buffer, data);
+				
+                t_pcb_memoria* proceso_recibido = obtener_proceso(solicitud.pid);
                 if (proceso_recibido == NULL) {
-                    log_error(memoria_logger, "Proceso no encontrado: PID %d", solicitud->pid);
-					eliminar_buffer(buffer);
+                    log_error(memoria_logger, "Proceso no encontrado: PID %d", solicitud.pid);
+                    eliminar_buffer(buffer);
                     break;
                 }
-                int resultado = escribir_memoria(proceso_recibido, solicitud -> num_pagina, solicitud -> offset, solicitud -> buffer, solicitud->tamanio);
+                int resultado = escribir_memoria(proceso_recibido,  solicitud.num_pagina, solicitud.offset, data, tamanio);
                 if (resultado == 0) {
                     log_info(memoria_logger, "Escritura exitosa: PID %d, Pagina %d", solicitud.pid, solicitud.num_pagina);
                 } else {
@@ -64,47 +78,51 @@ void atender_memoria_entradasalida(){
                 break; 
 
 			case SOLICITUD_LECTURA:
-                t_pcb_memoria solicitud;
+
                 buffer_desempaquetar(buffer, &solicitud);
-                t_pcb_memoria* proceso_recibido = obtener_proceso(solicitud.pid);
-                if (proceso == NULL) {
+                buffer_desempaquetar(buffer, &tamanio);
+
+                if (proceso_recibido == NULL) {
                     log_error(memoria_logger, "Proceso no encontrado: PID %d", solicitud.pid);
 					eliminar_buffer(buffer);
                     break;
                 }
+
                 t_buffer* buffer_lectura = crear_buffer();
-				buffer_lectura->size = solicitud.solicitud_buffer->size;
+				buffer_lectura->size = tamanio;
                 buffer_lectura->stream = malloc(buffer_lectura->size);
 
-                int resultado = leer_memoria(proceso_recibido, solicitud.solicitud_num_pagina, solicitud.solicitud_offset, buffer_lectura->stream, buffer_lectura->size);
-                if (resultado == 0) {
-                    enviar_datos(fd_entradasalida,buffer_lectura->stream, buffer_lectura->size);
+                int resultado_lectura = leer_memoria(proceso_recibido, solicitud.num_pagina, solicitud.offset, buffer_lectura->stream, buffer_lectura->size);
+                if (resultado_lectura == 0) {
+                    enviar_datos(fd_entradasalida, buffer_lectura->stream, buffer_lectura->size);
                     log_info(memoria_logger, "Lectura exitosa: PID %d, Pagina %d", solicitud.pid, solicitud.num_pagina);
                 } else {
                     log_error(memoria_logger, "Error en lectura: PID %d, Pagina %d", solicitud.pid, solicitud.num_pagina);
                 }
-                free(buffer);
+                eliminar_buffer(buffer);
+                free(buffer_lectura->stream);
 				free(buffer_lectura);
                 break; //lo mismo con el case anterior (este también esta en el coenxion.h)
 			case -1:
 				log_error(memoria_logger, "Se perdio la conexion con ENTRADASALIDA!");
 				continuar = 0;
 				break;
-                */
+
 			default:
 				log_warning(memoria_logger, "MEMORIA: Operacion desconocida recibida de ENTRADASALIDA");
 				break;
 		}
 	}
 }
-/*
+
 // Función para obtener un proceso por su PID
 t_pcb_memoria* obtener_proceso(int pid) {
-    // Iterar sobre la lista de procesos para encontrar el proceso con el PID dado
-    for (int i = 0; i < list_size(procesos); i++) { //por ahora "procesos" quedará así hasta arreglar con fran si queda así o armo una t_list de procesos
-        t_pcb_memoria* proceso = list_get(procesos, i);
-        if (proceso->pid == pid) {
-            return proceso;
+    extern t_pcb_memoria procesos[]; // Declaración del array de procesos
+    extern int cantidad_procesos;    // Declaración de la cantidad de procesos
+
+    for (int i = 0; i < cantidad_procesos; i++) {
+        if (procesos[i].pid == pid) {
+            return &procesos[i];
         }
     }
     // Si no se encuentra el proceso, retornar NULL
@@ -132,4 +150,3 @@ void enviar_datos(int socket, void* buffer, int size) { //todavía tengp que rev
         printf("No se pudieron enviar todos los datos\n");
     }
 } 
-*/
